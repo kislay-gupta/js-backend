0590c7cb3726cc959b480312bff4e35f891d0dc5
kislay-gupta <kislaygupta007@gmail.com>
2024-10-07 13:01:23 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // const app = express();
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed", err);
+  });
 
 /*
 (async () => {
