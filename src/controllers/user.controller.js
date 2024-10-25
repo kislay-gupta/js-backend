5b147dd2422783ec7e3cf9dd55bcb904d580ba47
kislay-gupta <kislaygupta007@gmail.com>
2024-10-25 13:25:10 +0530

    fixed all register controller code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,14 +17,24 @@ const registerUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(400, "All Fields required");
   }
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
     throw new ApiError(409, "User with email or username all ready exist");
   }
-  const avatarLocalPath = req.fields?.avatar[0]?.path;
-  const coverImageLocalPath = req.fields?.coverImage[0]?.path;
+  console.log(req.files);
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   if (!avatarLocalPath) throw new ApiError(400, "Avatar is required");
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
@@ -37,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage?.url || "",
     email,
     password,
-    username: username.loweCase(),
+    username: username.toLowerCase(),
   });
 
   const createUser = await User.findById(user._id).select(
